db5920a485cc9cb9f4f8b1e0e2da790729cd2001
Jonathan Mumm <mumm@tokbox.com>
2012-08-06 16:19:04 -0700

    Update OpenTok.cs
    Convert create_time param to int.

--- OpenTok.cs
+++ OpenTok.cs
@@ -51,7 +51,7 @@ namespace OpenTok
             NameValueCollection appSettings = ConfigurationManager.AppSettings;
 
             options.Add("session_id", sessionId);
-            options.Add("create_time", (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000);
+            options.Add("create_time", Convert.ToInt32(DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000));
             options.Add("nonce", RandomNumber(0, 999999));
             if (!options.ContainsKey(TokenPropertyConstants.ROLE))
             {
