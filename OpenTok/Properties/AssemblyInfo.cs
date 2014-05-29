e61706fcfff77d4038cf7d81798e7e1381f41de2
Ankur Oberoi <aoberoi@gmail.com>
2014-05-29 12:30:42 -0400

    update assembly info
    versioning best practices seen here: http://stackoverflow.com/questions/64602/what-are-differences-between-assemblyversion-assemblyfileversion-and-assemblyin

--- OpenTok/Properties/AssemblyInfo.cs
+++ OpenTok/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenTok")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OpenTok Server SDK")]
+[assembly: AssemblyDescription("OpenTok is an API from TokBox that enables websites to weave live group video communication into their online experience.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TokBox, Inc.")]
 [assembly: AssemblyProduct("OpenTok")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © TokBox, Inc. 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.*")]
