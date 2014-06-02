e814ca9b11fc6306486fbcc1a005695f6e520980
Ankur Oberoi <aoberoi@gmail.com>
2014-06-02 10:46:55 -0400

    add a simple HelloWorld app using Nancy
    this commit doesn't add any opentok functionality, just trying the
    framework.

---
+++ Samples/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://+:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Running on {0}", url);
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Samples/HelloWorld/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace HelloWorld
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
