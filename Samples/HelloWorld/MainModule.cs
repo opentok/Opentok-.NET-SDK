c74fe06d5d69cd73f52b94c71caaa4bc4684f101
Ankur Oberoi <aoberoi@gmail.com>
2014-06-03 10:23:08 -0400

    progress on nancy based helloworld sample

---
+++ Samples/HelloWorld/MainModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nancy;
+using OpenTokSDK;
+
+namespace HelloWorld
+{
+
+    public class MainModule : NancyModule
+    {
+
+        public MainModule(OpenTokService opentokService)
+        {
+
+            Get["/"] = _ =>
+                {
+                    var token = opentokService.Session.GenerateToken();
+                    var locals = new Dictionary<string, string>
+                    {
+                        { "Token", token },
+                        { "ApiKey", opentokService.OpenTok.ApiKey.ToString() },
+                        { "SessionId", opentokService.Session.Id }
+                    };
+
+                    return View["index", locals];
+                };
+        }
+    }
+}
---
+++ Samples/HelloWorld/OpenTokService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Configuration;
+using OpenTokSDK;
+
+namespace HelloWorld
+{
+    public class OpenTokService
+    {
+        public Session Session { get; protected set; }
+        public OpenTok OpenTok { get; protected set; }
+
+        public OpenTokService()
+        {
+            int apiKey = 0;
+            string apiSecret = null;
+            try
+            {
+                string apiKeyString = ConfigurationManager.AppSettings["API_KEY"];
+                apiSecret = ConfigurationManager.AppSettings["API_SECRET"];
+                apiKey = Convert.ToInt32(apiKeyString);
+            }
+
+            catch (Exception ex)
+            {
+                if (!(ex is ConfigurationErrorsException || ex is FormatException || ex is OverflowException))
+                {
+                    throw ex;
+                }
+            }
+
+            finally
+            {
+                if (apiKey == 0 || apiSecret == null)
+                {
+                    Console.WriteLine(
+                        "The OpenTok API Key and API Secret were not set in the application configuration. " +
+                        "Set the values in App.config and try again. (apiKey = {0}, apiSecret = {1})", apiKey, apiSecret);
+                    Console.ReadLine();
+                    Environment.Exit(-1);
+                }
+            }
+
+            this.OpenTok = new OpenTok(apiKey, apiSecret);
+
+            this.Session = this.OpenTok.CreateSession();
+        }
+    }
+}
