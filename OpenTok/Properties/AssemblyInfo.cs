274b9987bd24201d4eb2649a2a3f9cc4ef7ab8be
Manik Sachdeva <msach22@gmail.com>
2018-10-26 13:41:05 -0700

    Update to version 3.1.0

--- OpenTok/Properties/AssemblyInfo.cs
+++ OpenTok/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.*")]
-[assembly: AssemblyInformationalVersion("3.0.1-Alpha1")]
+[assembly: AssemblyVersion("3.1.*")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
