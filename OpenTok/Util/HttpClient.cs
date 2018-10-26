91950d1f4e62d8ee8ff5b91e94489db3a23f58ea
Manik Sachdeva <manik@tokbox.com>
2018-10-26 13:19:16 -0700

    fix http delete req issue (#119)

--- OpenTok/Util/HttpClient.cs
+++ OpenTok/Util/HttpClient.cs
@@ -69,10 +69,10 @@ namespace OpenTokSDK.Util
             return DoRequest(url, headers, data);
         }
 
-        public virtual string Delete(string url, Dictionary<string, string> headers, Dictionary<string, object> data)
+        public virtual string Delete(string url, Dictionary<string, string> headers)
         {
             headers.Add("Method", "DELETE");
-            return DoRequest(url, headers, data);
+            return DoRequest(url, headers, null);
         }
 
         public string DoRequest(string url, Dictionary<string, string> specificHeaders,
