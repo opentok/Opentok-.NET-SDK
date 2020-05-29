5900d9e8b2113bd9b33a614c01d8a596623c8010
Daniel Garcia <danigpino@gmail.com>
2020-05-29 16:48:09 +0200

    ported to .net standard 2.0

--- Samples/HelloWorld/MainModule.cs
+++ Samples/HelloWorld/MainModule.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Dynamic;
+﻿using System.Dynamic;
 
 using Nancy;
-using OpenTokSDK;
 
 namespace HelloWorld
 {
