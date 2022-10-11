540638304e7ba03170e91b250597324442ffd93b
Tr00d <guillaume.faas@live.fr>
2022-10-11 16:51:59 +0200

    Implement POST call to start a rendering

---
+++ OpenTok/OpenTok.Render.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OpenTokSDK.Render;
+
+namespace OpenTokSDK
+{
+    public partial class OpenTok
+    {
+        /// <summary>
+        ///     Indicates the StartRender endpoint (/render)
+        /// </summary>
+        public const string StartRenderEndpoint = "/render";
+
+        /// <summary>
+        ///     TODO
+        /// </summary>
+        /// <param name="request">TODO</param>
+        /// <returns>TODO</returns>
+        public async Task<StartRenderResponse> StartRenderAsync(StartRenderRequest request)
+        {
+            var response = await this.Client.PostAsync(
+                this.BuildUrl(StartRenderEndpoint),
+                GetHeaderDictionary("application/json"),
+                request.ToDataDictionary());
+            return JsonConvert.DeserializeObject<StartRenderResponse>(response);
+        }
+
+        private string BuildUrl(string endpoint) => $"v2/project/{ApiKey}{endpoint}";
+
+        private static Dictionary<string, string> GetHeaderDictionary(string contentType) =>
+            new Dictionary<string, string> {{"Content-Type", contentType}};
+    }
+}
---
+++ OpenTok/Render/StartRenderResponse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OpenTokSDK.Render
+{
+    /// <summary>
+    /// </summary>
+    public struct StartRenderResponse
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="sessionId"></param>
+        /// <param name="projectId"></param>
+        /// <param name="createdAt"></param>
+        /// <param name="updatedAt"></param>
+        /// <param name="url"></param>
+        /// <param name="resolution"></param>
+        /// <param name="status"></param>
+        /// <param name="streamId"></param>
+        public StartRenderResponse(string id, string sessionId, string projectId, int createdAt, int updatedAt, Uri url,
+            ScreenResolution resolution, string status, string streamId)
+        {
+            this.Id = id;
+            this.SessionId = sessionId;
+            this.ProjectId = projectId;
+            this.CreatedAt = createdAt;
+            this.UpdatedAt = updatedAt;
+            this.Url = url;
+            this.Resolution = resolution;
+            this.Status = status;
+            this.StreamId = streamId;
+        }
+
+        public string Id { get; set; }
+
+        public string SessionId { get; set; }
+
+        public string ProjectId { get; set; }
+
+        public int CreatedAt { get; set; }
+
+        public int UpdatedAt { get; set; }
+
+        public Uri Url { get; set; }
+
+        public ScreenResolution Resolution { get; set; }
+
+        public string Status { get; set; }
+
+        public string StreamId { get; set; }
+    }
+}
