e688ddffa08d4c60d592f86fe0a8b0f0071baa95
Tr00d <guillaume.faas@live.fr>
2022-11-10 08:39:38 +0100

    Add missing Xml comments

--- OpenTok/Render/RenderItem.cs
+++ OpenTok/Render/RenderItem.cs
@@ -4,23 +4,23 @@ using Newtonsoft.Json;
 namespace OpenTokSDK.Render
 {
     /// <summary>
-    /// TODO
+    /// Represents a rendering.
     /// </summary>
     public struct RenderItem
     {
         /// <summary>
-        /// TODO
+        /// Constructor.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="sessionId"></param>
-        /// <param name="projectId"></param>
-        /// <param name="createdAt"></param>
-        /// <param name="updatedAt"></param>
-        /// <param name="url"></param>
-        /// <param name="resolution"></param>
-        /// <param name="status"></param>
-        /// <param name="streamId"></param>
-        /// <param name="reason"></param>
+        /// <param name="id">The Id of the rendering.</param>
+        /// <param name="sessionId">The Id of the session.</param>
+        /// <param name="projectId">The Id of the project.</param>
+        /// <param name="createdAt">The creation date.</param>
+        /// <param name="updatedAt">The last update date.</param>
+        /// <param name="url">The Url.</param>
+        /// <param name="resolution">The screen resolution.</param>
+        /// <param name="status">The status.</param>
+        /// <param name="streamId">The Id of the stream.</param>
+        /// <param name="reason">The reason.</param>
         public RenderItem(string id, string sessionId, string projectId, int createdAt, int updatedAt, Uri url,
             ScreenResolution resolution, string status, string streamId, string reason)
         {
@@ -37,53 +37,53 @@ namespace OpenTokSDK.Render
         }
 
         /// <summary>
-        /// TODO
+        /// The Id of the rendering.
         /// </summary>
         public string Id { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The Id of the session.
         /// </summary>
         public string SessionId { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The Id of the project.
         /// </summary>
         public string ProjectId { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The creation date.
         /// </summary>
         public double CreatedAt { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The last update date.
         /// </summary>
         public double UpdatedAt { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The Url.
         /// </summary>
         public Uri Url { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The screen resolution.
         /// </summary>
         [JsonConverter(typeof(ScreenResolutionConverter))]
         public ScreenResolution Resolution { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The status.
         /// </summary>
         public string Status { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The Id of the stream.
         /// </summary>
         public string StreamId { get; set; }
 
         /// <summary>
-        /// TODO
+        /// The reason.
         /// </summary>
         public string Reason { get; set; }
     }
