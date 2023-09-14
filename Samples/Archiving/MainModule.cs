759eae84f2d4ba65b34c5350877e30d3d3ec6948
tr00d <guillaume.faas@live.fr>
2023-09-14 07:36:43 +0200

    Update packages in Archiving sample app

--- Samples/Archiving/MainModule.cs
+++ Samples/Archiving/MainModule.cs
@@ -1,91 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Dynamic;
-
+﻿using System.Dynamic;
 using Nancy;
 using OpenTokSDK;
 
 namespace Archiving
 {
-
-    public class MainModule : NancyModule
-    {
-
-        public MainModule(OpenTokService opentokService)
-        {
-
-            Get["/"] = _ => View["index"];
-
-            Get["/host"] = _ =>
-            {
-                dynamic locals = new ExpandoObject();
-
-                locals.ApiKey = opentokService.OpenTok.ApiKey.ToString();
-                locals.SessionId = opentokService.Session.Id;
-                locals.Token = opentokService.Session.GenerateToken();
-
-                return View["host", locals];
-            };
-
-            Get["/participant"] = _ =>
-            {
-                dynamic locals = new ExpandoObject();
-
-                locals.ApiKey = opentokService.OpenTok.ApiKey.ToString();
-                locals.SessionId = opentokService.Session.Id;
-                locals.Token = opentokService.Session.GenerateToken();
-
-                return View["participant", locals];
-            };
-
-            Get["/history"] = _ =>
-            {
-                var page = Request.Query.page.HasValue ? (int)Request.Query.page : 1;
-                var offset = (page - 1) * 5;
-                ArchiveList archives = opentokService.OpenTok.ListArchives(offset, 5);
-
-                string showPrevious = page > 1 ? "/history?page=" + (page - 1).ToString() : null;
-                string showNext = archives.TotalCount > (offset + 5) ? "/history?page=" + (page + 1).ToString() : null;
-
-                dynamic locals = new ExpandoObject();
-                locals.Archives = archives;
-                locals.ShowPrevious = showPrevious;
-                locals.ShowNext = showNext;
-
-                return View["history", locals];
-            };
-
-            Get["/download/{id}"] = parameters =>
-            {
-                Archive archive = opentokService.OpenTok.GetArchive(parameters.id);
-                return Response.AsRedirect(archive.Url);
-            };
-
-            Post["/start"] = _ =>
-            {
-                Archive archive = opentokService.OpenTok.StartArchive(
-                    opentokService.Session.Id,
-                    name: ".NET Archiving Sample App",
-                    hasAudio: (Boolean)(this.Request.Form.hasAudio),
-                    hasVideo: (Boolean)(this.Request.Form.hasVideo),
-                    outputMode: (this.Request.Form.outputMode == "composed" ? OutputMode.COMPOSED : OutputMode.INDIVIDUAL)
-                );
-                return archive;
-            };
-
-            Get["/stop/{id}"] = parameters =>
-            {
-                Archive archive = opentokService.OpenTok.StopArchive(parameters.id);
-                return archive;
-            };
-
-            Get["/delete/{id}"] = parameters =>
-            {
-                opentokService.OpenTok.DeleteArchive(parameters.id);
-                return Response.AsRedirect("/history");
-            };
-        }
-    }
+	public class MainModule : NancyModule
+	{
+		public MainModule(OpenTokService opentokService)
+		{
+			Get("/", _ => View["index"]);
+			Get("/host", _ =>
+			{
+				dynamic locals = new ExpandoObject();
+				locals.ApiKey = opentokService.OpenTok.ApiKey.ToString();
+				locals.SessionId = opentokService.Session.Id;
+				locals.Token = opentokService.Session.GenerateToken();
+				return View["host", locals];
+			});
+			Get("/participant", _ =>
+			{
+				dynamic locals = new ExpandoObject();
+				locals.ApiKey = opentokService.OpenTok.ApiKey.ToString();
+				locals.SessionId = opentokService.Session.Id;
+				locals.Token = opentokService.Session.GenerateToken();
+				return View["participant", locals];
+			});
+			Get("/history", _ =>
+			{
+				var page = Request.Query.page.HasValue ? (int) Request.Query.page : 1;
+				var offset = (page - 1) * 5;
+				var archives = opentokService.OpenTok.ListArchives(offset, 5);
+				var showPrevious = page > 1 ? "/history?page=" + (page - 1) : null;
+				var showNext = archives.TotalCount > offset + 5 ? "/history?page=" + (page + 1) : null;
+				dynamic locals = new ExpandoObject();
+				locals.Archives = archives;
+				locals.ShowPrevious = showPrevious;
+				locals.ShowNext = showNext;
+				return View["history", locals];
+			});
+			Get("/download/{id}", parameters =>
+			{
+				Archive archive = opentokService.OpenTok.GetArchive(parameters.id);
+				return Response.AsRedirect(archive.Url);
+			});
+			Post("/start", _ =>
+			{
+				var archive = opentokService.OpenTok.StartArchive(
+					opentokService.Session.Id,
+					".NET Archiving Sample App",
+					hasAudio: (bool) this.Request.Form.hasAudio,
+					hasVideo: (bool) this.Request.Form.hasVideo,
+					outputMode: this.Request.Form.outputMode == "composed" ? OutputMode.COMPOSED : OutputMode.INDIVIDUAL
+				);
+				return archive;
+			});
+			Get("/stop/{id}", parameters =>
+			{
+				Archive archive = opentokService.OpenTok.StopArchive(parameters.id);
+				return archive;
+			});
+			Get("/delete/{id}", parameters =>
+			{
+				opentokService.OpenTok.DeleteArchive(parameters.id);
+				return Response.AsRedirect("/history");
+			});
+		}
+	}
 }
